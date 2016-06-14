998cf3fdaaa9596035b27617aa28dcc9d3df1614
chiongwah <cheng@ChengChiongWah.(none)>
2016-06-14 17:51:42 +0800

    1606141751

--- spyder.py
+++ spyder.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 import urllib2
 import re, sys
 
@@ -7,7 +8,7 @@ class Spider_Model(object):
         self.name = []
 
     def GetPage(self):
-        MyUrl = 'https://www.zhihu.com/people/xie-ke-41'
+        MyUrl = 'http://www.zhihu.com/people/kong-qing-xun'
 	UserAgent = 'Mozilla/4.0(compatible; MSIE 5.5; Windows NT)'
 	Headers = {'User-Agent': UserAgent }
 	req = urllib2.Request(MyUrl, headers=Headers)
@@ -20,11 +21,12 @@ class Spider_Model(object):
 	Location = re.findall(r'<span class="location.*?><a.*?>(.*?)</a></span>', UnicodePage, re.S)
 	Business_Item = re.findall(r'<span class="business item".*?><a.*?>(.*?)</a></span>', UnicodePage, re.S)
 	Employment_Item = re.findall(r'<span class="employment item".*?><a.*?>(.*?)</a></span>', UnicodePage, re.S)
-	Education_Item = re.findall(r'<span class="education item".*?>(.*?)</span>.|\n<span class="education-extra.*?>(.*?)</span>', UnicodePage, re.S)
+	Education_Item = re.findall(r'<span class="education item" title="(.*?)">.*?</span>', UnicodePage, re.S)
+	Education_Extra_Item = re.findall(r'<span class="education-extra item" title=["\'](.*?)[\'"]>.*?</span>', UnicodePage, re.S)
 	Description = re.findall(r'<span class="description unfold.*?">\n<span class="cont.*?>\n\n(.*?)\n\n</span>', UnicodePage, re.S)
-	return Description, #Education_Item, Employment_Item, Business_Item, Location, ''.join(Title_Section[0])
+	print Title_Section[0], '\n', Location[0], '\n', Business_Item, '\n', Employment_Item[0], '\n', Education_Item[0], '\n', Education_Extra_Item, '\n', Description
 
 Spyder = Spider_Model()
 
 if __name__ == '__main__':
-    print Spyder.GetPage()
+    Spyder.GetPage()
