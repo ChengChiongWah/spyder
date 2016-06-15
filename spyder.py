168b0a4f212efee6f5de0f7028534df1cbb6ae1d
chiongwah <cheng@ChengChiongWah.(none)>
2016-06-15 18:02:05 +0800

    1606151801

--- spyder.py
+++ spyder.py
@@ -9,13 +9,16 @@ class Spider_Model(object):
 
     def GetPage(self):
         MyUrl = 'http://www.zhihu.com/people/kong-qing-xun'
-	UserAgent = 'Mozilla/4.0(compatible; MSIE 5.5; Windows NT)'
+	UserAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0'
 	Headers = {'User-Agent': UserAgent }
 	req = urllib2.Request(MyUrl, headers=Headers)
 	MyResponse = urllib2.urlopen(req)
 	MyPage = MyResponse.read()
 
 	UnicodePage = MyPage.decode('utf-8')
+	Text = file("txt.txt", "w+")
+	Text.write(MyPage)
+	Text.close()
         
 	Title_Section = re.findall(r'<span class="name">(.*?)</span>(.*?)<span class="bio.*?>(.*?)</span>', UnicodePage, re.S)
 	Location = re.findall(r'<span class="location.*?><a.*?>(.*?)</a></span>', UnicodePage, re.S)
@@ -24,7 +27,8 @@ class Spider_Model(object):
 	Education_Item = re.findall(r'<span class="education item" title="(.*?)">.*?</span>', UnicodePage, re.S)
 	Education_Extra_Item = re.findall(r'<span class="education-extra item" title=["\'](.*?)[\'"]>.*?</span>', UnicodePage, re.S)
 	Description = re.findall(r'<span class="description unfold.*?">\n<span class="cont.*?>\n\n(.*?)\n\n</span>', UnicodePage, re.S)
-	print Title_Section[0], '\n', Location[0], '\n', Business_Item, '\n', Employment_Item[0], '\n', Education_Item[0], '\n', Education_Extra_Item, '\n', Description
+	People = re.findall(r'<a class="author-link" data-tip=.*?href=".*?people/(.*?)">', UnicodePage, re.S) 
+	print Title_Section[0], '\n', Location[0], '\n', Business_Item, '\n', Employment_Item[0], '\n', Education_Item[0], '\n', Education_Extra_Item, '\n', Description, '\n', People
 
 Spyder = Spider_Model()
 
