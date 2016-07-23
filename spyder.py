e659c732af1559d1b384707276d19bf1627f11aa
cheng <cheng@localhost.localdomain>
2016-07-23 14:14:58 +0800

    1607231414

--- spyder.py
+++ spyder.py
@@ -10,7 +10,6 @@ from bs4 import BeautifulSoup
 
 
 def spyder(people, parent, level):
-    
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     fh=logging.handlers.RotatingFileHandler('log.txt', maxBytes=1024*1024, backupCount=1)
@@ -34,10 +33,10 @@ def spyder(people, parent, level):
         cur = conn.cursor()
         if len(Description) == 0 :
             cur.execute("insert into People_Inf values(?, ?, ?, ?, ?)", (Title_Section[0], None, people, parent, level)) 
-            print Title_Section[0], None, people[:], parent, level,
+            print Title_Section[0], None, people[:], parent, level,"-------"
         else:
             cur.execute("insert into People_Inf values(?, ?, ?, ?, ?)", (Title_Section[0], Description[0], people, parent, level))
-	    print Title_Section[0], Description[0], people[:]
+	    print Title_Section[0], Description[0], people[:], parent, level,"--------"
 #	    cur.execute("delete from People where name = ?", (people))
         cur.close()
         conn.commit()
@@ -47,22 +46,35 @@ def spyder(people, parent, level):
         for people_list in list(set(People)):
             conn2 = sqlite3.connect("./spyder.db")
 	    cur2 = conn2.cursor()
-	    cur2.execute('select count(*) from People_Inf where Name_Url=?', (people_list,))
+	    cur2.execute('select count(*) from People where Name=?', (people_list,))
 	    cur2_len =  cur2.fetchall()[0][0]
 	    if cur2_len == 0:
 	        cur2.execute("insert into People values(?, ?, ?, ?)", (people, level+1, False, people_list,))
-		print people_list
+		#print people_list
 		cur2.close()
 		conn2.commit()
 		conn2.close()
 	
-	conn3 = sqlite3.connect("./spyder.db")
-	cur3 = conn3.cursor()
-	cur3 = execute("select Name from People where Level=(select min(Level) from People) and Done=0")
-	cur3_list = cur3.fetchall()
-	spyder(people_list)
     except BaseException, e:
         logger.debug('there is a error', exc_info=True) 
+
+def run():
+    while True:
+        conn3 = sqlite3.connect("./spyder.db")
+	cur3 = conn3.cursor()
+        cur3.execute("select Name, Parent, Level from People where Level=(select min(Level) from People) and Done=0")
+	cur3_list = cur3.fetchall()
+	cur3.close()
+	conn3.close()
+	for element in cur3_list:
+	    print element[0]#, element[1], element[2]
+	    conn4 = sqlite3.connect("./spyder.db")
+	    cur4 = conn4.cursor()
+	    cur4.execute("update People set Done=1 where Name=?", (element[0],))
+	    cur4.close()
+	    conn4.commit()
+	    conn4.close()
+	    spyder(element[0], element[1], element[2])
 
 if __name__ == '__main__':
     if os.path.exists('./spyder.db'):
@@ -70,3 +82,4 @@ if __name__ == '__main__':
     else:
         DB_Sqlite3.Create_Db()
     spyder('li-de-long-68', 'root', 0)
+    run()
